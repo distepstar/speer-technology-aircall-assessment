fac87080b6b941601608939f9bc1b50c666c972a
Louis Lei <jobsdzihinss@gmail.com>
2022-08-31 17:40:03 -0400

    heroku commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 8081;
+
+
+const app = express();
+
+
+app.use(express.static(__dirname));
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+
+})
+
+app.listen(port);
+console.log("Server started")
